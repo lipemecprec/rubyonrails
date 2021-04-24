c0023b9ae594763be54a70b017e182a59bc551d3
lipemecprec <lipemecprec@yahoo.com.br>
2021-04-23 21:52:58 -0300

    Comments crud

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do
+    # get "/articles", to: "articles#index"
+    # get "/articles/:id", to: "articles#show"
+    resources :comments
+  end
  
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
